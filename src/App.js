088b60060627267a8f73d3c213a3a9deb29b8f5b
Grzegorz Patyk <grzegorz.patyk@ttms.pl>
2022-06-17 21:42:07 +0200

    move useSelector hook and state display to main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './features/counter/Counter';
+import { useSelector } from 'react-redux';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const count = useSelector((state) => state.counter.value);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    Count: <span>{count}</span>
+                </h1>
+                <Counter />
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { decrement, increment } from './counterSlice';
 
 export default function Counter() {
-    const count = useSelector((state) => state.counter.value);
     const dispatch = useDispatch();
 
     return (
-        <div>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                width: '50%',
+            }}
+        >
             <button
                 aria-label="Increment value"
                 onClick={() => dispatch(increment())}
             >
                 Increment
             </button>
-            <span>{count}</span>
             <button
                 aria-label="Decrement value"
                 onClick={() => dispatch(decrement())}
